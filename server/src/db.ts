3b1004e67bc5fd45df472d793ad25b62b95094c6
Christian64 <christianabreuh@gmail.com>
2023-07-04 10:36:51 -0400

    Fix: removing logs in DB.

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,7 +1,4 @@
 import { connect } from "mongoose";
-console.log("Database");
-console.log(process.env.DB_URI);
-console.log("Database 2");
 if (process.env.DB_URI) {
   connect(process.env.DB_URI);
 }
